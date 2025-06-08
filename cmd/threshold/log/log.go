a303b5bd53e2c187900773f28295f5a97b70a8d4
xq.yan <yanxiangqun@gmail.com>
2025-06-08 18:57:45 +0800

    Remove caller key from log encoder configuration

--- cmd/threshold/log/log.go
+++ cmd/threshold/log/log.go
@@ -13,6 +13,7 @@ var (
 func Init(level string) {
 	cfg := zap.NewProductionConfig()
 	cfg.EncoderConfig.TimeKey = ""
+	cfg.EncoderConfig.CallerKey = ""
 	zapLevel, parseErr := zap.ParseAtomicLevel(level)
 	if parseErr != nil {
 		panic("Failed to parse log level: " + parseErr.Error())
